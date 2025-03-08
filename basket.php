2eaeb1583e7f32fa79271de902593b5a257b117e
kushan-panda <kushan.20230023@iit.ac.lk>
2025-03-08 12:16:06 +0530

    Assigent variables to get the productid and quantity that user added to basket

--- basket.php
+++ basket.php
@@ -1,5 +1,6 @@
 <?php
-$pagename="template"; //Create and populate a variable called $pagename
+include("db.php"); //include db.php file to connect to DB
+$pagename="Smart Basket"; //Create and populate a variable called $pagename
 
 echo "<link rel=stylesheet type=text/css href=mystylesheet.css>"; //Call in stylesheet
 echo "<title>".$pagename."</title>"; //display name of the page as window title
@@ -10,6 +11,15 @@ include ("headfile.html"); //include header layout file
 
 echo "<h4>".$pagename."</h4>"; //display name of the page on the web page
 //display random text
+
+$newprodid=$_POST['h_prodid'];
+//capture the required quantity of selected product using the POST method and $_POST superglobal variable
+//and store it in a new local variable called $reququantity
+$reququantity=$_POST['p_quantity'];
+//Display id of selected product
+echo "<p>Id of selected product: ".$newprodid;
+//Display quantity of selected product
+echo "<br>Quantity of selected product: ".$reququantity;
 
 
 include("footfile.html"); //include head layout
